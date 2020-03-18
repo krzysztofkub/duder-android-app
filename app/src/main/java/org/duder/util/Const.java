eccd78bedafeb36a2f601e4e2cc0e9a5a695d32a
Kubieniec <krzysztof.kubieniec@hotmail.com>
2020-03-18 18:13:08 +0100

    new destination address

--- app/src/main/java/org/duder/util/Const.java
+++ app/src/main/java/org/duder/util/Const.java
@@ -2,8 +2,10 @@ package org.duder.util;
 
 public class Const {
 	public static final String TAG = "duder";
-	public static final String WS_ADDRESS = "ws://18.225.31.141:8080/ws/websocket";
-	public static final String REST_ADDRESS = "http://18.225.31.141:8080";
+
+	public static final String IP_ADDRESS = "18.219.45.180";
+	public static final String WS_ADDRESS = "ws://" + IP_ADDRESS + ":8080/ws/websocket";
+	public static final String REST_ADDRESS = "http://" + IP_ADDRESS + ":8080";
 
 	public static final String WS_SEND_MESSAGE_ENDPOINT = "/app/chat.sendMessage";
 	public static final String TOPIC_PUBLIC = "/topic/public";
