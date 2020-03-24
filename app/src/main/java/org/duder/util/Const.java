1728f49a4bc11bbcb043aa98308cb1196c1c0239
Kubieniec <krzysztof.kubieniec@hotmail.com>
2020-03-24 21:03:33 +0100

    new message endpoint

--- app/src/main/java/org/duder/util/Const.java
+++ app/src/main/java/org/duder/util/Const.java
@@ -4,11 +4,11 @@ public class Const {
 	public static final String TAG = "duder";
 
 	public static final String IP_ADDRESS = "18.219.45.180";
-	public static final String WS_ADDRESS = "ws://" + IP_ADDRESS + ":8080/ws/websocket";
-	public static final String REST_ADDRESS = "http://" + IP_ADDRESS + ":8080";
 
-	public static final String WS_SEND_MESSAGE_ENDPOINT = "/app/chat.sendMessage";
+	public static final String WS_ADDRESS = "ws://" + IP_ADDRESS + ":8080/ws/websocket";
+	public static final String WS_SEND_MESSAGE_ENDPOINT = "/app/sendMessage";
 	public static final String TOPIC_PUBLIC = "/topic/public";
 
+	public static final String REST_ADDRESS = "http://" + IP_ADDRESS + ":8080";
 	public static final String GET_MESSAGE_HISTORY_ENDPOINT = "/getChatState";
 }
